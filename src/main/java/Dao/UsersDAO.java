0ddc08729ba726b02edbf3dce17f8f49b444c3d4
246093 <246093@WINDOWS-10TMLQ5.mr.hi-joho.ac.jp>
2026-05-20 09:29:53 +0900

    Users

--- src/main/java/Dao/UsersDAO.java
+++ src/main/java/Dao/UsersDAO.java
@@ -1,13 +1,45 @@
 package Dao;
 import java.sql.Connection;
-import java.sql.*;
-import java.util.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Model.UsersBean;
 
 public class UsersDAO extends DAOBase{
 	public List<UsersBean> findByName(UsersBean usersBean){
-		List<UsersBean> usersList = new ArrayList<>
-		
+		List<UsersBean> usersList = new ArrayList<>();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めません");
+		}
+		try(Connection conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
+			String sql = "SELECT * FROM USERS WHERE NAME LIKE ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, "%" + usersBean.getUserName() + "%");
+			
+			try(ResultSet rs = pStmt.executeQuery()){
+				while(rs.next()) {
+					UsersBean u = new UsersBean();
+					u.setUserId(rs.getInt("USER_ID"));
+					u.setUserName(rs.getString("USER_NAME"));
+					u.setTel(rs.getString("TEL"));
+					u.setLoginId(rs.getInt("LOGIN_ID"));
+					u.setPassword(rs.getString("PASSWORD"));
+					u.setUserClass(rs.getString("USER_CLASS"));
+					u.setUserStatus(rs.getString("USRE_STATUS"));
+					u.setUserRegist(rs.getDate("USER_REGIST"));
+					u.setUserUpdate(rs.getDate("USER_UPDATE"));
+					
+				}
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return usersList;
 	}
 }

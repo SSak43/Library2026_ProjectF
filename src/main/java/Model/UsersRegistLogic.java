9e2c8d72e83ac05575af7552dd1d0973f9a06558
246093 <246093@WINDOWS-10TMLQ5.mr.hi-joho.ac.jp>
2026-06-01 09:48:55 +0900

    利用者登録処理完成。

--- src/main/java/Model/UsersRegistLogic.java
+++ src/main/java/Model/UsersRegistLogic.java
@@ -1,11 +1,9 @@
 package Model;
 
-import java.util.List;
-
 import Dao.UsersRegistDAO;
 
 public class UsersRegistLogic {
-	public List<UsersBean> add(UsersBean usersBean){
+	public boolean add(UsersBean usersBean){
 		UsersRegistDAO dao = new UsersRegistDAO();
 		return dao.add(usersBean);
 	}
--- src/main/java/Servlet/UsersRegistServlet.java
+++ src/main/java/Servlet/UsersRegistServlet.java
@@ -2,6 +2,7 @@ package Servlet;
 
 import java.io.IOException;
 
+import Model.UsersBean;
 import Model.UsersRegistLogic;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -14,38 +15,61 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 public class UsersRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UsersRegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UsersRegistServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// 1. Logicを呼び出して最新のIDを取得する
-         UsersRegistLogic logic = new UsersRegistLogic(); 
-         int latestId = logic.getLatestId();
-        
-        // 2. 取得したIDを request にセットしてJSPに渡す
-        request.setAttribute("latestId", latestId);
-        
-        // 3. 登録画面（JSP）へフォワード
-        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/UsersRegist.jsp");
-        dispatcher.forward(request, response);		
-		
+		UsersRegistLogic logic = new UsersRegistLogic();
+		int latestId = logic.getLatestId();
+
+		// 2. 取得したIDを request にセットしてJSPに渡す
+		request.setAttribute("latestId", latestId);
+
+		// 3. 登録画面（JSP）へフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/UsersRegist.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 入力データ受け取る
+		String cla = request.getParameter("cla");
+		String name = request.getParameter("userName");
+		String tel = request.getParameter("Tel");
+		String pass = request.getParameter("Password");
+		//　受け取ったデータをセット
+		UsersBean usersBean = new UsersBean();
+		usersBean.setUserClass(cla);
+		usersBean.setUserName(name);
+		usersBean.setTel(tel);
+		usersBean.setPassword(pass);
+
+		//データベースへ登録
+		UsersRegistLogic logic = new UsersRegistLogic();
+		boolean Add = logic.add(usersBean);
+
+		if (Add) {
+			response.sendRedirect("UsersMainServlet");
+		} else {
+			request.setAttribute("errorMsg", "登録に失敗しました");
+		}
+
 	}
 
 }

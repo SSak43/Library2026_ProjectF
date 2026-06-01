63819e73ca83ee14b2afb995a8046a7b762c9dc9
246014 <246014@WINDOWS-6QI92A6.mr.hi-joho.ac.jp>
2026-06-01 11:51:45 +0900

    修正

--- src/main/java/Model/BooksBean.java
+++ src/main/java/Model/BooksBean.java
@@ -3,37 +3,38 @@ package Model;
 import java.sql.Date;
 
 public class BooksBean {
-    private int bookId;           // 図書ID
-    private String title;         // 書名
-    private String writerName;    // 著者
-    private String company;       // 出版社
-    private String bookClass;     // 分類
-    private String bookStatus;    // 状態（0:貸出可能, 1:貸出中, 2:貸出不可）
-    private Date bookRegist;      // 登録日
-    private Date bookUpdate;      // 更新日
+    private int bookId;          // 図書ID
+    private String title;        // 書名
+    private String writerName;   // 著者
+    private String company;      // 出版社
+    private String bookClass;    // 分類
+    private String bookStatus;   // 状態 (0:貸出可能, 1:貸出中, 2:貸出不可)
+    private Date bookRegist;     // 登録日
+    private Date bookUpdate;     // 更新日
 
-    // 以下、ゲッターとセッター
+ 
+
     public int getBookId() { return bookId; }
-    public void setBookId(int bookId) { this bookId = bookId; }
+    public void setBookId(int bookId) { this.bookId = bookId; }
 
     public String getTitle() { return title; }
-    public void setTitle(String title) { this title = title; }
+    public void setTitle(String title) { this.title = title; }
 
     public String getWriterName() { return writerName; }
-    public void setWriterName(String writerName) { this writerName = writerName; }
+    public void setWriterName(String writerName) { this.writerName = writerName; }
 
     public String getCompany() { return company; }
-    public void setCompany(String company) { this company = company; }
+    public void setCompany(String company) { this.company = company; }
 
     public String getBookClass() { return bookClass; }
-    public void setBookClass(String bookClass) { this bookClass = bookClass; }
+    public void setBookClass(String bookClass) { this.bookClass = bookClass; }
 
     public String getBookStatus() { return bookStatus; }
-    public void setBookStatus(String bookStatus) { this bookStatus = bookStatus; }
+    public void setBookStatus(String bookStatus) { this.bookStatus = bookStatus; }
 
     public Date getBookRegist() { return bookRegist; }
-    public void setBookRegist(Date bookRegist) { this bookRegist = bookRegist; }
+    public void setBookRegist(Date bookRegist) { this.bookRegist = bookRegist; }
 
     public Date getBookUpdate() { return bookUpdate; }
-    public void setBookUpdate(Date bookUpdate) { this bookUpdate = bookUpdate; }
+    public void setBookUpdate(Date bookUpdate) { this.bookUpdate = bookUpdate; }
 }

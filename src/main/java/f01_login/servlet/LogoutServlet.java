999645269ecb7ca08b8695925b2aa92e1c4b390a
246014 <246014@WINDOWS-NK7CPMT.mr.hi-joho.ac.jp>
2026-06-04 10:10:39 +0900

    ログアウトボタン修正

--- src/main/java/f01_login/servlet/LogoutServlet.java
+++ src/main/java/f01_login/servlet/LogoutServlet.java
@@ -23,6 +23,6 @@ public class LogoutServlet extends HttpServlet {
 		}
 		
 		// 3. ログイン画面
-		response.sendRedirect("login.jsp");
+		response.sendRedirect(request.getContextPath() + "/login");
 	}
 }
